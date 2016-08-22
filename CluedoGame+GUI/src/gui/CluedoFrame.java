8e1cf59a926431ee040e70ff524ce819d27c0464
Aurabelle <nordic.almalexia@gmail.com>
2016-08-22 13:23:28 +1200

    Added dialog menus and methods which aid player creation/game init.

--- CluedoGame+GUI/src/gui/CluedoFrame.java
+++ CluedoGame+GUI/src/gui/CluedoFrame.java
@@ -2,19 +2,33 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.util.ArrayList;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
+
+import game.Player;
 
 /**
  * CluedoFrame is what it says on the box: a frame for holding all the elements 
@@ -28,13 +42,16 @@ import javax.swing.JTextArea;
  * Credit to Maxim Shoustin (http://stackoverflow.com/questions/13057836/using-two-jpanels-in-one-jframe)
  * for the example using JSplitPanes, which really helped me figure out some
  * important Swing concepts.
+ * This tutorial was also very useful when creating the JMenuBar (http://docs.oracle.com/javase/tutorial/uiswing/components/menu.html),
+ * and this site for the JRadioButtons with JDialogs (http://myy.haaga-helia.fi/~atk84d/tutorial/uiswing/mini/fifthexample.html)
+ * 
  * Credit also to DJP for the MoonLander tutorial exercise, and the author of the 
  * COMP261 GUIs used with the Maps and 3D Rendering programs.
  * 
  * @author Marielle
  *
  */
-public class CluedoFrame extends Subject{
+public class CluedoFrame extends Subject implements WindowListener, MouseListener{
 	//required UID for JFrame extension
 	private static final long serialVersionUID = 1L;
 	
@@ -44,6 +61,7 @@ public class CluedoFrame extends Subject{
 	//protected abstract void displayPlayerDetails(Graphics g);
 	//protected abstract void displayPlayerHand(Graphics g);
 	//protected abstract void displayBoard(Graphics g);
+	//protected abstract void redraw(Graphics g)?
 	//protected abstract void onButtonPress(String action);
 	//protected abstract void onMouseClick(MouseEvent me);
 	
@@ -64,21 +82,34 @@ public class CluedoFrame extends Subject{
 	private JSplitPane boardAndCards;	// board and player's hand
 	
 	//actual display components
+	private JMenuBar menuBar;
 	private JPanel userButtons;
 	private CluedoCanvas canvas;
-	private JPanel playerJournal;
+	private JPanel journalPanel;
 	private JPanel playerStats;
 	private JPanel cardDisplay;
+	private JTextArea journalText;
+	
+	private JFrame popUpFrame;
+	
+	//helpers for player initialization
+	ArrayList<String> possibleTokens;
+	String token;
 	
 	/**
 	 * Creates a new instance of a Cluedo GUI
 	 */
 	public CluedoFrame(){
 		super("Cluedo");
-
+		initialise();
+	}
+	
+	private void initialise(){
+		populateTokenArray();
 		createUserButtons();
 		createBoardCardsPane();
 		createInfoPane();
+		createMenuBar();
 		
 		completePane = new JSplitPane();
 		completePane.setLeftComponent(userButtons);
@@ -86,11 +117,18 @@ public class CluedoFrame extends Subject{
 		
 		setLayout(new BorderLayout());
 		setPreferredSize(totalSize);
+		add(menuBar, BorderLayout.NORTH);
 		add(completePane, BorderLayout.CENTER);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);		
+		
+		popUpFrame = new JFrame();
+		
+		//closing is now handled by windowClosing()
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(this);
+		
 		setResizable(false);		//for now, until I actually get something working
-		//pack();
-		//setVisible(true);
+		pack();
+		setVisible(true);
 	}
 	
 	/**
@@ -166,14 +204,15 @@ public class CluedoFrame extends Subject{
 			}
 		};
 		playerStats.setPreferredSize(playerDetailSize);
-		playerJournal = new JPanel();
-	    playerJournal.setLayout(new BorderLayout());
+		journalPanel = new JPanel();
+	    journalPanel.setLayout(new BorderLayout());
 
-	    playerJournal.add(new JLabel( "Notes:" ), BorderLayout.NORTH);
-	    playerJournal.add(new JTextArea(), BorderLayout.CENTER);
+	    journalPanel.add(new JLabel( "Notes:" ), BorderLayout.NORTH);
+	    journalText = new JTextArea();
+	    journalPanel.add(journalText, BorderLayout.CENTER);
 	    
 	    playerInfoPane.setTopComponent(playerStats);
-	    playerInfoPane.setBottomComponent(playerJournal);
+	    playerInfoPane.setBottomComponent(journalPanel);
 	    
 	    mainPlayerInterface.setLeftComponent(boardAndCards);
 	    mainPlayerInterface.setRightComponent(playerInfoPane);
@@ -181,7 +220,13 @@ public class CluedoFrame extends Subject{
 	
 	private void createBoardCardsPane(){
 		canvas = new CluedoCanvas();
-		canvas.setPreferredSize(canvasSize);	
+		canvas.setPreferredSize(canvasSize);
+		canvas.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				//onMouseClick(e);
+				redraw();
+			}
+		});
 		
 		cardDisplay = new JPanel(){
 			@Override
@@ -198,11 +243,211 @@ public class CluedoFrame extends Subject{
 		boardAndCards.setBottomComponent(cardDisplay);
 	}
 
-	
-	public static void main( String args[] ){
-	    // Create an instance of the test application
-	    CluedoFrame mainFrame = new CluedoFrame();
-	    mainFrame.pack();
-	    mainFrame.setVisible( true );
+	private void createMenuBar(){
+		menuBar = new JMenuBar();
+		
+		JMenu fileMenu = new JMenu("File");
+		
+		JMenuItem newGameOpt = new JMenuItem("New Game");
+		newGameOpt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ev) {
+				//onButtonPress("new");
+			}
+		});
+		fileMenu.add(newGameOpt);
+		fileMenu.add(new JMenuItem("Exit"));
+		
+		menuBar.add(fileMenu);
 	}
+
+	/**
+	 * This creates a new dialog window which returns the number of players
+	 * required by the current game
+	 */
+	public int getNumPlayers(){
+		Object[] possibleNums = {3, 4, 5, 6};
+		int numPlayers = (Integer)JOptionPane.showInputDialog(
+                popUpFrame,
+                "Please select the total number of players for this game:",
+                "Set Number of Players",
+                JOptionPane.PLAIN_MESSAGE,
+                null, possibleNums, 3);
+
+		if(numPlayers > 2){
+			//onButtonPress("numPlayers");
+			initialise();
+		}
+		return numPlayers;
+	}
+	
+	public String getPlayerName(){
+		String name = (String)JOptionPane.showInputDialog(
+                popUpFrame,
+                "Please enter your name",
+                "Set Player Name",
+                JOptionPane.PLAIN_MESSAGE,
+                null, null, "");
+		if ((name != null) && (name.length() > 0)) {
+			return name;
+		}
+		else{
+			return "Player";
+		}
+	}
+	
+	public String getPlayerToken(){
+		popUpFrame = new JFrame("Choose Player Character");
+		// Create the components for the popup
+        JLabel title = new JLabel("Click the \"Okay\" button once you have selected your desired character.",
+                JLabel.CENTER);
+        JPanel choicePanel = createTokenChoiceDialog();
+        
+        // Set the layout.
+        popUpFrame.setLayout(new BorderLayout());
+        popUpFrame.add(title, BorderLayout.NORTH);      
+        popUpFrame.add(choicePanel, BorderLayout.CENTER);
+
+        // Exit when the window is closed.
+        popUpFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+
+        popUpFrame.pack();
+        popUpFrame.setVisible(true); 
+
+		return token;
+	}
+	
+	private JPanel createTokenChoiceDialog(){
+		//create an actionListener which does nothing when the 
+		//button is selected, since we want that to happen AFTER
+		//the dialog is okayed to close
+		ActionListener redundant = new ActionListener(){
+			public void actionPerformed(ActionEvent e) {}
+		};
+		//create the buttons, add the actionListener, and add each one to the 
+		//buttonGroup IF it's still in the possible selections array
+		JRadioButton[] options = new JRadioButton[possibleTokens.size()];
+		ButtonGroup group = new ButtonGroup();
+		
+		 for(int i = 0; i < possibleTokens.size(); i++){
+			 System.out.println(possibleTokens.get(i));
+			 //if this is the first button added, set it as selected (to avoid not having something selected when 
+			 //the dialog is exited)
+			 //This method should not be called if all 6 players have already been initialized
+			 JRadioButton jrb; 
+			 if(i == 0){
+				 jrb = new JRadioButton(possibleTokens.get(i), true);
+				 jrb.setActionCommand(possibleTokens.get(i));
+				 
+			 }
+			 else{
+				 jrb = new JRadioButton(possibleTokens.get(i));
+				 jrb.setActionCommand(possibleTokens.get(i));
+			 }
+			 jrb.addActionListener(redundant);
+			 options[i] = jrb;
+			 group.add(jrb);
+		 }
+		 
+		 //create the button to confirm selection
+		 JButton confirm = new JButton("Okay");
+		 confirm.addActionListener(new ActionListener() {
+			@Override	
+			 public void actionPerformed(ActionEvent ev) {
+					//find out which JRadioButton is selected, get value,
+					//remove value from list of tokens and return it
+					String selected = group.getSelection().getActionCommand();
+					for(int i = 0; i < possibleTokens.size(); i++){
+						if(possibleTokens.get(i).equals(selected)){
+							possibleTokens.remove(i);
+							token = selected; 
+						}
+					}
+				}
+			});
+		 
+		 return createTokenPane("Avaliable characters:", options, confirm);
+	}
+	
+	private JPanel createTokenPane(String description, JRadioButton[] options, JButton confirmButton){
+		JPanel radioOptions = new JPanel();
+        JLabel label = new JLabel(description);
+
+        radioOptions.setLayout(new BorderLayout());
+        radioOptions.add(label);
+
+        for (int i = 0; i < options.length; i++) {
+            radioOptions.add(options[i]);
+        }
+
+        JPanel pane = new JPanel();
+        pane.setLayout(new BorderLayout());
+        pane.add(radioOptions, BorderLayout.NORTH);
+        pane.add(confirmButton, BorderLayout.SOUTH);
+
+        return pane;
+	}
+	
+	private void populateTokenArray(){
+		String[] tokens = {"Miss Scarlet", "Colonel Mustard", "Mrs White", "Mr Green", "Mrs Peacock", "Professor Plum"};
+		possibleTokens = new ArrayList<String>();
+		for(String s: tokens){
+			possibleTokens.add(s);
+		}
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		//onMouseClick(me)
+	}
+	
+	public void redraw(){
+		this.repaint();
+	}
+	
+	public JTextArea getJournal(){
+		return journalText;
+	}
+	
+	/**
+	 * This method is called when the user clicks on the "X" button in the
+	 * right-hand corner.
+	 *
+	 * @param e
+	 * @author David J Pearce : thanks :D
+	 */
+ 	public void windowClosing(WindowEvent e) {
+		// Ask the user to confirm they wanted to do this
+		int r = JOptionPane.showConfirmDialog(this, new JLabel(
+		"Exit Cluedo?"), "Confirm Exit",
+		JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+		if (r == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
+	}
+	
+ 	public static void main(String args[]){
+ 		CluedoFrame cf = new CluedoFrame();
+ 		System.out.println(cf.getPlayerName());
+ 		System.out.println(cf.getPlayerToken());
+ 	}
+	/*************************************************************************
+	 * The following methods are required for the Window/MouseListener 
+	 * interfaces but there is no reason to implement them at this stage. 
+	 * @param e / me
+	 ************************************************************************/
+    public void windowClosed(WindowEvent e) {}
+    public void windowActivated(WindowEvent e) {}
+    public void windowDeactivated(WindowEvent e) {}
+    public void windowDeiconified(WindowEvent e) {}
+    public void windowIconified(WindowEvent e) {}
+    public void windowOpened(WindowEvent e) {}
+
+	@Override
+	public void mouseEntered(MouseEvent me) {}
+	@Override
+	public void mouseExited(MouseEvent me) {}
+	@Override
+	public void mousePressed(MouseEvent me) {}
+	@Override
+	public void mouseReleased(MouseEvent me) {}
 }
